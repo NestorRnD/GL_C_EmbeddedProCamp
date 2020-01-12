047fd387ef06f2e3be58ed29b6c0fda0bf9d8e8e
Nestor Tyshko <tyshko.n@gmail.com>
2020-01-12 17:31:41 +0200

    improve makefile, add solution for imagined task

--- Lecture_9/FRTOS_inc/FreeRTOSConfig.h
+++ Lecture_9/FRTOS_inc/FreeRTOSConfig.h
@@ -30,12 +30,14 @@
 #define configUSE_TICK_HOOK                      0
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
-#define configMAX_PRIORITIES                     ( 7 )
+#define configMAX_PRIORITIES                     ( 11 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE                    ((size_t)3072)
+#define configTOTAL_HEAP_SIZE                    ((size_t)8192)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configUSE_16_BIT_TICKS                   0
 #define configUSE_MUTEXES                        1
+#define configUSE_RECURSIVE_MUTEXES              1
+#define configUSE_COUNTING_SEMAPHORES            0
 #define configQUEUE_REGISTRY_SIZE                8
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION  1
 
@@ -49,7 +51,7 @@ to exclude the API function. */
 #define INCLUDE_uxTaskPriorityGet           1
 #define INCLUDE_vTaskDelete                 1
 #define INCLUDE_vTaskCleanUpResources       0
-#define INCLUDE_vTaskSuspend                1
+#define INCLUDE_vTaskSuspend                0
 #define INCLUDE_vTaskDelayUntil             0
 #define INCLUDE_vTaskDelay                  1
 #define INCLUDE_xTaskGetSchedulerState      1
--- Lecture_9/exercise9.c
+++ Lecture_9/exercise9.c
@@ -2,39 +2,112 @@
 
 #include <FreeRTOS.h>
 #include <task.h>
+#include <semphr.h>
 
 #include <stm32f30x.h>
 
-
+#define BUTTON	0
 // Number of leds
 #define NLEDS 8
 // every 4bits is port number conected to led
-#define ALEDS 0xABCDEF89
+#define ALEDS 0x8FEDCBA9
 
-int BlinkOn=0;
+#define DELAY 2000000
+
+volatile int LedsBlink=0; // 0 - no led blink
+uint64_t idleTicks=0;
 
 void vInitAll()
 {
-    RCC -> AHBENR |= RCC_AHBENR_GPIOEEN;
+    uint32_t mask=0;
+    int i;
 
-    // Enable PORTE pin 8 as digital output
-    GPIOE->MODER &= ~3<<(LED<<1);
-    GPIOE->MODER |= 1<<(LED<<1);
+    // Enable CLK for ports A and E 
+    RCC -> AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOEEN;
+
+    // Configure BUTTON pin (port A) as input
+    GPIOA->MODER &= ~3<<(BUTTON<<1);
+    GPIOA->PUPDR &= ~3<<(BUTTON<<1); // no pull-up/pull-down
+
+    // Configure PORTE pins as digital output
+    for(i=0;i<NLEDS;i++)
+	mask|=3<<(((ALEDS>>(i<<2))&0xF)<<1);
+    GPIOE->MODER &= ~mask;
+    mask=0;
+    for(i=0;i<NLEDS;i++)
+	mask|=1<<(((ALEDS>>(i<<2))&0xF)<<1);
+    GPIOE->MODER |= mask;
+}
+
+void myDelay(uint32_t n)
+{
+    volatile uint32_t i;
+    for(i=n;i>0;i--)
+	__asm__("nop");
 }
 
 void vLedTask (void *pvParameters)
 {
-    int i;
+    uint32_t i,led;
+
+    led=*((int*)pvParameters);
+    led= 1<<((ALEDS>>(led<<2))&0xF);
+
     for(i=0;i<10;i++){
-	GPIOE -> ODR ^= 1<<LED;
-	vTaskDelay(500);
+	GPIOE -> ODR ^= led;
+	myDelay(DELAY);
+    }
+    taskENTER_CRITICAL();
+    LedsBlink--;
+    taskEXIT_CRITICAL();
+    vTaskDelete(NULL);
+}
+
+void ErrorTask ()
+{
+    uint32_t i,leds=0;
+
+    for(i=0;i<NLEDS;i++)
+	leds |= 1<<((ALEDS>>(i<<2))&0xF);
+
+    while(1) {
+	GPIOE -> ODR ^= leds;
+	myDelay(1000000);
     }
 }
 
-int main()
+void vButtonTask (void *pvParameters)
 {
+    uint32_t ledIndex;
+    BaseType_t ret;
+    while(1) {
+	if((LedsBlink<8) && (GPIOA->IDR & (1<<BUTTON))){ // Button pressed
+	    taskENTER_CRITICAL();
+	    if(LedsBlink<8){
+		ledIndex=LedsBlink++;
+		ret=xTaskCreate(vLedTask, (char*)"LedTask", configMINIMAL_STACK_SIZE,
+			&ledIndex, tskIDLE_PRIORITY+LedsBlink, NULL);
+		if(ret!=pdPASS) ErrorTask();
+	    }
+	    taskEXIT_CRITICAL();
+	    vTaskDelay(200);
+	}
+	vTaskDelay(100);
+    }
+}
+
+int main ()
+{
+    BaseType_t ret;
     vInitAll();
-    xTaskCreate(vLedTask, (char*)"LedTask", configMINIMAL_STACK_SIZE,
-		NULL, tskIDLE_PRIORITY + 1, NULL);
-    vTaskStartScheduler();
+
+    ret=xTaskCreate(vButtonTask, (char*)"ButtonTask", configMINIMAL_STACK_SIZE, NULL, 10, NULL);
+    if(ret!=pdPASS)
+	ErrorTask();
+    else
+	vTaskStartScheduler();
+}
+
+void vApplicationIdleHook( void ){
+    idleTicks++;
 }
